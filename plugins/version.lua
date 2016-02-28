0d738ac0c6f93c15d4c29a8d2035643a37c14491
MeGaTG <arabiamir13@gmail.com>
2016-02-28 06:04:56 -0800

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,7 +14,20 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
+  return 'MeGaTG Bot V1
+  
+  Supergroups advanced Administration bot based on yagop/telegram-bot 
+  
+  Developers :
+  @arabi373
+  
+  Special thanks to:
+  
+  @ShhbDaRk
+  
+  @BehroozYaghi
+  
+  
   --[[ Checkout https://github.com/Josepdal/DBTeam
   --GNU GPL v2 license.]]
 end
